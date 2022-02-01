7926d55639f6a8cb9012301194740b43256f404b
danik <=>
2022-02-01 13:56:32 +0200

    point

--- Git_visual_test/Source.cpp
+++ Git_visual_test/Source.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+using namespace std;
+
+class point {
+	int x, y, z;
+public:
+	point() { x = y = z; }
+	void print() { cout << x << " : " << y << " : " << z << "\n"; }
+};
 
 int main() {
+	
+	point p;
+	p.print();
+	return 0;
 
 	return 0;
 }   
